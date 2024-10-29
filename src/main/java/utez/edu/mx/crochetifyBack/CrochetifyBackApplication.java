157be8ffadbf84dcbc98d26677c83dd304d075f9
abrahamramirezT <104937972+abrahamramirezT@users.noreply.github.com>
2024-10-28 23:34:46 -0600

    first commit

---
+++ src/main/java/utez/edu/mx/crochetifyBack/CrochetifyBackApplication.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.crochetifyBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrochetifyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrochetifyBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utez/edu/mx/crochetifyBack/CrochetifyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package utez.edu.mx.crochetifyBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrochetifyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
